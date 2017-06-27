4a1f07bfab65266bb32f49f1dcbed78fb5a1117f
ccorrigan <charlie.ps.corrigan@gmail.com>
2017-06-26 19:12:54 -0600

    Create transactions show request spec

--- spec/requests/api/v1/transactions_request_spec.rb
+++ spec/requests/api/v1/transactions_request_spec.rb
@@ -14,4 +14,21 @@ describe "Transactions API" do
       expect(transactions.count).to eq(3)
     end
   end
+
+  context "GET /api/v1/transactions" do
+    it "sends a single transaction by id" do
+      id = create(:transaction).id
+
+      get "/api/v1/transactions/#{:id}"
+
+      expect(response).to be_success
+
+      transaction = JSON.parse(response.body)
+
+      expect(transaction["id"]).to eq(id)
+      expect(transaction).to have_key("invoice_id")
+      expect(transaction).to have_key("credit_card_number")
+      expect(transaction).to have_key("result")
+    end
+  end
 end
