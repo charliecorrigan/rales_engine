0d112c1266b68508bd66729dfca52349953af7c7
ccorrigan <charlie.ps.corrigan@gmail.com>
2017-06-26 20:03:35 -0600

    Create customers#index route

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170627011450) do
+ActiveRecord::Schema.define(version: 20170627015717) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "customers", force: :cascade do |t|
+    t.string "last_name"
+    t.string "first_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "merchants", force: :cascade do |t|
     t.string "name"
--- spec/requests/api/v1/customers_request_spec.rb
+++ spec/requests/api/v1/customers_request_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 describe "Customers API" do
   context "GET /api/v1/customers" do
     it "sends a list of customers" do
-      create_list(:customers, 3)
+      create_list(:customer, 3)
 
       get '/api/v1/customers'
 
