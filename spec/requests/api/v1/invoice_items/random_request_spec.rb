82eac36b336fa6d22e8b12643274b06888f57afe
Chris Smith <42@chrisryansmith.com>
2017-06-27 17:20:14 -0600

    initial invoice_item random spec

--- spec/requests/api/v1/invoice_items/random_request_spec.rb
+++ spec/requests/api/v1/invoice_items/random_request_spec.rb
@@ -1,16 +1,16 @@
 require 'rails_helper'
 
-describe 'Items API' do
-  context 'GET /api/v1/item/random' do
-    it 'returns a random item' do
-      items = create_list(:item, 3)
-      ids = items.map(&:id)
+describe 'Invoice_Items API' do
+  context 'GET /api/v1/invoice_item/random' do
+    it 'returns a random invoice_item' do
+      invoice_items = create_list(:invoice_item, 3)
+      ids = invoice_items.map(&:id)
 
-      get "/api/v1/items/random"
+      get "/api/v1/invoice_items/random"
 
-      raw_item = JSON.parse(response.body)
+      raw_invoice_item = JSON.parse(response.body)
 
-      expect(ids.include?(raw_item['id'])).to be true
+      expect(ids.include?(raw_invoice_item['id'])).to be true
     end
   end
 end
