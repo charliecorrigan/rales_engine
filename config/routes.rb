4dc15514b1ac5d375b24bdc4ef64452ca1228845
ccorrigan <charlie.ps.corrigan@gmail.com>
2017-06-27 14:21:00 -0600

    Create find and find_all routes, actions, etc under merchants

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,18 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :merchants, only: [:index, :show]
-      resources :transactions, only: [:index, :show]
       namespace :customers do
         get '/find_all', to: "find#index"
         get '/find', to: "find#show"
       end
 
+      namespace :merchants do
+        get '/find_all', to: "find#index"
+        get '/find', to: "find#show"
+      end
+
+      resources :transactions, only: [:index, :show]
+      resources :merchants, only: [:index, :show]
       resources :customers, only: [:index, :show]
 
     end
--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -29,5 +29,128 @@ describe "Merchants API" do
       expect(merchant).to have_key("name")
     end
   end
+
+  ##################################################
+  ##################################################
+
+  context "GET /api/v1/merchants/find_all?name=?" do
+    it "sends a list of merchants with included name" do
+      merchant1 = create(:merchant, name: "Bob Store")
+      merchant2 = create(:merchant, name: "Bob Store")
+      merchant3 = create(:merchant, name: "Bill Store")
+
+      get "/api/v1/merchants/find_all?name=#{merchant1.name}"
+
+      expect(response).to be_success
+
+      merchants = JSON.parse(response.body)
+
+      expect(merchants.count).to eq(2)
+    end
+  end
+
+  context "GET /api/v1/merchants/find_all?id=?" do
+    it "sends a list of merchants with included id" do
+      merchants = create_list(:merchant, 3)
+
+      get "/api/v1/merchants/find_all?id=#{merchants.first.id}"
+      expect(response).to be_success
+
+      merchant_results = JSON.parse(response.body)
+
+      expect(merchant_results.count).to eq(1)
+    end
+  end
+
+  context "GET /api/v1/merchants/find_all?created_at=?" do
+    it "sends a list of merchants with included created_at" do
+      merchant1 = create(:merchant, created_at: "2012-03-27T14:56:35.000Z")
+      create(:merchant, created_at: "2012-03-27T14:56:35.000Z")
+      create(:merchant, created_at: "2012-03-27T14:56:35.000Z")
+      create(:merchant)
+
+      get "/api/v1/merchants/find_all?created_at=#{merchant1.created_at}"
+      expect(response).to be_success
+
+      merchant_results = JSON.parse(response.body)
+
+      expect(merchant_results.count).to eq(3)
+    end
+  end
+
+  context "GET /api/v1/merchants/find_all?updated_at=?" do
+    it "sends a list of merchants with included updated_at" do
+      merchant1 = create(:merchant, updated_at: "2012-03-27T14:56:35.000Z")
+      create(:merchant, updated_at: "2012-03-27T14:56:35.000Z")
+      create(:merchant, updated_at: "2012-03-27T14:56:35.000Z")
+      create(:merchant)
+
+      get "/api/v1/merchants/find_all?updated_at=#{merchant1.updated_at}"
+      expect(response).to be_success
+
+      merchant_results = JSON.parse(response.body)
+
+      expect(merchant_results.count).to eq(3)
+    end
+  end
+
+  context "GET /api/v1/merchants/find?name=?" do
+    it "sends first merchant with included name" do
+      merchant1 = create(:merchant, name: "Bob Store")
+      merchant2 = create(:merchant, name: "Rob Store")
+      merchant3 = create(:merchant, name: "Bill Store")
+      get "/api/v1/merchants/find?name=#{merchant1.name}"
+
+      expect(response).to be_success
+      merchant_result = JSON.parse(response.body)
+
+      expect(merchant_result["id"]).to eq(merchant1.id)
+    end
+  end
+
+  context "GET /api/v1/merchants/find?id=?" do
+    it "sends first merchant with included id" do
+      merchants = create_list(:merchant, 3)
+
+      get "/api/v1/merchants/find?id=#{merchants.first.id}"
+      expect(response).to be_success
+
+      merchant_result = JSON.parse(response.body)
+
+      expect(merchant_result["id"]).to eq(merchants.first.id)
+    end
+  end
+
+  context "GET /api/v1/merchants/find?created_at=?" do
+    it "sends first merchant with included created_at" do
+      merchant1 = create(:merchant, created_at: "2012-03-27T14:56:35.000Z")
+      create(:merchant)
+      create(:merchant)
+      create(:merchant)
+
+      get "/api/v1/merchants/find?created_at=#{merchant1.created_at}"
+      expect(response).to be_success
+
+      merchant_result = JSON.parse(response.body)
+
+      expect(merchant_result["id"]).to eq(merchant1.id)
+    end
+  end
+
+  context "GET /api/v1/merchants/find?updated_at=?" do
+    it "sends first merchant with included updated_at" do
+      merchant1 = create(:merchant, updated_at: "2012-03-27T14:56:35.000Z")
+      create(:merchant)
+      create(:merchant)
+      create(:merchant)
+
+      get "/api/v1/merchants/find?updated_at=#{merchant1.updated_at}"
+      expect(response).to be_success
+
+      merchant_result = JSON.parse(response.body)
+
+      expect(merchant_result["id"]).to eq(merchant1.id)
+    end
+  end
 end
 
