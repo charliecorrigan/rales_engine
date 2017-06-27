ba278d92efb430803abbe75f6b490e07ef2e3cab
Chris Smith <42@chrisryansmith.com>
2017-06-27 14:27:48 -0600

    replace invoice views with jbuilder

--- app/controllers/api/v1/invoices_controller.rb
+++ app/controllers/api/v1/invoices_controller.rb
@@ -1,32 +1,12 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> skip failing date search spec
 module Api
   module V1
     class InvoicesController < ApplicationController
       def index
-<<<<<<< HEAD
         @invoices = Invoice.all
       end
 
       def show
         @invoice = Invoice.find(params[:id])
-      end
-    end
-=======
-class Api::V1::InvoicesController < ApplicationController
-  def index
-    render json: Invoice.all
->>>>>>> complete invoices index response endpoint
-  end
-=======
-        render json: Invoice.all
-      end
->>>>>>> skip failing date search spec
-
-      def show
-        render json: Invoice.find(params[:id])
       end
     end
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  namespace :api do
+  namespace :api, defaults: {format: :json} do
     namespace :v1 do
       namespace :customers do
         get '/find_all', to: "find#index"
