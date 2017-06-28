6ca146d7bbe9375b5a81dae6cdab045f889e9ab2
ccorrigan <charlie.ps.corrigan@gmail.com>
2017-06-28 08:09:52 -0600

    Create query, method, view for merchant revenue

--- app/controllers/api/v1/merchants/revenue_controller.rb
+++ app/controllers/api/v1/merchants/revenue_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::Merchants::RevenueController < ApplicationController
   def show
-
+    @revenue = Merchant.revenue(params[:id])
   end
 end
--- spec/requests/api/v1/merchants/revenue_request_spec.rb
+++ spec/requests/api/v1/merchants/revenue_request_spec.rb
@@ -4,11 +4,19 @@ describe "Merchants Revenue API" do
   context 'GET /api/v1/merchants/:id/revenue' do
     it "sends the total revenue of a single merchant" do
       merchant1 = create(:merchant, id: 1)
-      create(:invoice_with_invoice_items, merchant_id: 1)
+      item1 = create(:item, merchant: merchant1)
+      item2 = create(:item, merchant: merchant1)
+      invoice1 = create(:invoice, merchant: merchant1)
+      invoice_items1 = create(:invoice_item, item: item1, invoice: invoice1)
+      invoice_items2 = create(:invoice_item, item: item2, quantity: 2, invoice: invoice1)
 
       get "/api/v1/merchants/#{merchant1.id}/revenue"
 
       expect(response).to be_success
+
+      revenue_result = JSON.parse(response.body)
+
+      expect(revenue_result).to eq({"revenue"=>3})
     end
   end
 end
