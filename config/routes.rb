0b6e8d0e24edb7bfa196724b6b1e07cc9293c5f7
ccorrigan <charlie.ps.corrigan@gmail.com>
2017-06-28 17:39:54 -0600

    Create routes for invoice relationship endpoints

--- config/routes.rb
+++ config/routes.rb
@@ -31,6 +31,11 @@ Rails.application.routes.draw do
         get "/find", to: "find#show"
         get "/find_all", to: "find#index"
         get "/random", to: "random#show"
+        get '/:id/customer', to: "customer#show"
+        get '/:id/invoice_items', to: "invoice_items#index"
+        get '/:id/items', to: "items#index"
+        get '/:id/merchant', to: "merchant#show"
+        get '/:id/transactions', to: "transactions#index"
       end
       resources :invoices, only: [:index, :show]
 
