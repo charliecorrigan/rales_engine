b44045478a62dc6427f80e29a4fb4f92b18f99ea
ccorrigan <charlie.ps.corrigan@gmail.com>
2017-06-29 12:30:11 -0600

    Create route for customer favorite merchant

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,10 @@ Rails.application.routes.draw do
         get '/find_all', to: "find#index"
         get '/find', to: "find#show"
         get '/random', to: "random#show"
+
         get '/:id/invoices', to: "invoices#index"
         get '/:id/transactions', to: "transactions#index"
+        get '/:id/favorite_merchant', to: "favorite_merchant#show"
       end
       resources :customers, only: [:index, :show]
 
