f3eaf33067932ba733b97328f65274a25887b52d
ccorrigan <charlie.ps.corrigan@gmail.com>
2017-06-28 18:27:34 -0600

    Create invoice_items relationship endpoints

--- config/routes.rb
+++ config/routes.rb
@@ -52,6 +52,8 @@ Rails.application.routes.draw do
         get "/find", to: "find#show"
         get "/find_all", to: "find#index"
         get "/random", to: "random#show"
+        get '/:id/item', to: "item#show"
+        get '/:id/invoice', to: "invoice#show"
       end
       resources :invoice_items, only: [:index, :show]
     end
--- spec/requests/api/v1/invoice_items/invoice_relationship_spec.rb
+++ spec/requests/api/v1/invoice_items/invoice_relationship_spec.rb
@@ -13,10 +13,12 @@ describe 'Invoice_item Invoice API' do
 
       expect(raw_invoice).to have_key('id')
       expect(raw_invoice['id']).to be_a Integer
-      expect(raw_invoice).to have_key('name')
-      expect(raw_invoice['name']).to be_a String
-
-      expect(raw_invoice['id']).to eq invoice.id
+      expect(raw_invoice).to have_key('customer_id')
+      expect(raw_invoice['customer_id']).to be_a Integer
+      expect(raw_invoice).to have_key('merchant_id')
+      expect(raw_invoice['merchant_id']).to be_a Integer
+      expect(raw_invoice).to have_key('status')
+      expect(raw_invoice['status']).to be_a String
     end
   end
 end
