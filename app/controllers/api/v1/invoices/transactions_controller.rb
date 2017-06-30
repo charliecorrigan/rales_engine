d7a51f3e1db34d5e96c020a866e74cc82c78bac2
ccorrigan <charlie.ps.corrigan@gmail.com>
2017-06-30 07:50:18 -0600

    Reroute path and delete duplicate views for invoices transactions controller

--- app/controllers/api/v1/invoices/transactions_controller.rb
+++ app/controllers/api/v1/invoices/transactions_controller.rb
@@ -2,5 +2,6 @@ class Api::V1::Invoices::TransactionsController < ApplicationController
   def index
     invoice = Invoice.find(params[:id])
     @transactions = invoice.transactions
+    render 'api/v1/transactions/index'
   end
 end
