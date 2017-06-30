04a3e20f6a9c2d595b9fbae34c39d1571e6b5eaf
Chris Smith <42@chrisryansmith.com>
2017-06-29 18:26:13 -0600

    refactor Merchants Revenue API

--- spec/requests/api/v1/merchants/bi/revenue_request_spec.rb
+++ spec/requests/api/v1/merchants/bi/revenue_request_spec.rb
@@ -1,34 +1,29 @@
 require 'rails_helper'
 
 describe "Merchants Revenue API" do
+  let(:merchant1) { create(:merchant) }
+  let(:invoice1) { create(:invoice, :with_transactions, merchant: merchant1, created_at: "2017-06-28 00:00:00") }
+  let(:invoice2) { create(:invoice, :with_transactions, merchant: merchant1, created_at: "2017-06-29 00:00:00") }
+
+  before do
+    create_list(:invoice_item, 3, invoice: invoice1)
+    create_list(:invoice_item, 3, invoice: invoice2)
+  end
+
   context 'GET /api/v1/merchants/:id/revenue' do
     it "sends the total revenue of a single merchant" do
-      merchant1 = create(:merchant, id: 1)
-      invoice1 = create(:invoice, merchant: merchant1)
-      invoice_items1 = create(:invoice_item, invoice: invoice1)
-      invoice_items2 = create(:invoice_item, quantity: 2, invoice: invoice1)
-      create(:transaction, invoice: invoice1)
       get "/api/v1/merchants/#{merchant1.id}/revenue"
 
       expect(response).to be_success
 
       revenue_result = JSON.parse(response.body)
 
-      expect(revenue_result).to eq({"revenue"=>"0.03"})
+      expect(revenue_result).to eq({"revenue"=>"0.06"})
     end
   end
 
-    context 'GET /api/v1/merchants/:id/revenue' do
-    it "sends the total revenue of a single merchant" do
-      merchant1 = create(:merchant, id: 1)
-      invoice1 = create(:invoice, merchant: merchant1, created_at: "2017-06-28 00:00:00")
-      invoice_items1 = create(:invoice_item, invoice: invoice1)
-      invoice_items2 = create(:invoice_item, quantity: 2, invoice: invoice1)
-      create(:transaction, invoice: invoice1)
-      invoice2 = create(:invoice, merchant: merchant1, created_at: "2017-06-29 00:00:00")
-      invoice_items1 = create(:invoice_item, invoice: invoice2)
-      invoice_items2 = create(:invoice_item, quantity: 2, invoice: invoice2)
-      create(:transaction, invoice: invoice2)
+  context 'GET /api/v1/merchants/:id/revenue?date=x' do
+    it "sends the total revenue of a single merchant on a specific date" do
       get "/api/v1/merchants/#{merchant1.id}/revenue?date=2017-06-28 00:00:00"
 
       expect(response).to be_success
