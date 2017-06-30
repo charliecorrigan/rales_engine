8473a9c934efad6e1d8efbb7fadec0cd2bd7d588
ccorrigan <charlie.ps.corrigan@gmail.com>
2017-06-30 07:56:36 -0600

    Reroute path and delete duplicate views for items merchant controller

--- app/controllers/api/v1/items/merchant_controller.rb
+++ app/controllers/api/v1/items/merchant_controller.rb
@@ -2,5 +2,6 @@ class Api::V1::Items::MerchantController < ApplicationController
   def show
     item = Item.find(params[:id])
     @merchant = item.merchant
+    render 'api/v1/merchant/show'
   end
 end
