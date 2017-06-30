97da4b96f807ac712608d7cd29c6b49f0e1aaf18
ccorrigan <charlie.ps.corrigan@gmail.com>
2017-06-30 07:38:40 -0600

    Reroute path and delete duplicate views for invoices customer controller

--- app/controllers/api/v1/invoices/customer_controller.rb
+++ app/controllers/api/v1/invoices/customer_controller.rb
@@ -2,5 +2,6 @@ class Api::V1::Invoices::CustomerController < ApplicationController
   def show
     invoice = Invoice.find(params[:id])
     @customer = invoice.customer
+    render 'api/v1/customers/show'
   end
 end
--- app/views/api/v1/invoices/customer/show.json.jbuilder
+++
@@ -1,1 +0,0 @@
-json.partial! 'api/v1/customers/partials/customer', customer: @customer
