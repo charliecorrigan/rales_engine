471115108c9100d314ce705828aa3eefc43ad93a
ccorrigan <charlie.ps.corrigan@gmail.com>
2017-06-30 07:34:10 -0600

    Reroute path and delete duplicate views for invoice_items item controller

--- app/controllers/api/v1/invoice_items/invoice_controller.rb
+++ app/controllers/api/v1/invoice_items/invoice_controller.rb
@@ -2,5 +2,6 @@ class Api::V1::InvoiceItems::InvoiceController < ApplicationController
   def show
     invoice_item = InvoiceItem.find(params[:id])
     @invoice = invoice_item.invoice
+    render 'api/v1/invoices/show'
   end
 end
